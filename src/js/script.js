3d13dae13d6870096a87972c09a1b715dfa48136
Rimma <teststudy1311@gmail.com>
2019-05-30 23:38:09 +0600

    added modal close after 5seconds

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,18 @@ let btn = document.querySelector('#button'),
 	modal = document.querySelector('.modal'),
 	btnClose = document.querySelector('#close');
 
-btn.addEventListener('click', ()=>{
-	modal.classList.add('modal_active');
-});
+function closeModal(){
+	modal.classList.remove('modal_active');
+}
 
 btnClose.addEventListener('click', ()=>{
-	modal.classList.remove('modal_active');
+	closeModal();
 });
+
+btn.addEventListener('click', ()=>{
+	modal.classList.add('modal_active');
+	setTimeout(closeModal, 5000);
+});
+
+
 
