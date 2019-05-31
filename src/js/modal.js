3d24496e57ce6fdcf979ddd4f799197dfb8412b4
Rimma <teststudy1311@gmail.com>
2019-05-31 10:57:31 +0600

    added and used  jquery library

---
+++ src/js/modal.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	let btn = $('#button'),
+			modal = $('.modal'),
+			close = $('#close');
+	console.log(btn);
+
+	btn.on('click', ()=> {
+		modal.addClass('modal_active');
+	});
+
+	close.on('click', ()=>{
+		modal.removeClass('modal_active');
+	});
+
+
+});
