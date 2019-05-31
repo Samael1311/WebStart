ba703a6213f0b6e7606cc0b24230b99e8e473ada
Rimma <teststudy1311@gmail.com>
2019-05-31 19:30:40 +0600

    button1

--- src/js/toUpPage.js
+++ src/js/toUpPage.js
@@ -1,23 +1,25 @@
-$(document).ready(()=>{
+$(document).ready(() => {
 
 	let btnUp = $('#up');
 
-	btnUp.click(()=> {
-		$("html, body").animate({ scrollTop: 0 }, "slow");
+	btnUp.click(() => {
+		$("html, body").animate({
+			scrollTop: 0
+		}, "slow");
 
 	});
 
-		$(window).scroll(()=>{
-			if($(window).scrollTop() == $(document).height() - $(window).height()){
-				btnUp.addClass('up_active');
-				console.log('конец');
-			} else {
-				btnUp.removeClass('up_active');
-			}
-				
-			
-		});
-	
+	$(window).scroll(() => {
+		if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+			btnUp.addClass('up_active');
+			console.log('конец');
+		} else {
+			btnUp.removeClass('up_active');
+		}
+
+
+	});
+
 
 
 })
